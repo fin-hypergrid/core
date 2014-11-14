b04c6f89efb9aa572f92ba6adcd024632c09f987
steve wirts <stevewirts@gmail.com>
2014-11-14 14:27:22 -0500

    model set on front page

--- fin-hypergrid.js
+++ fin-hypergrid.js
@@ -5,9 +5,6 @@
     Polymer('fin-hypergrid', { /* jshint ignore:line  */
         ready: function() {
             root.fin.wc.hypergrid.Hypergrid(this);
-            //this.setBehavior(new root.fin.wc.hypergrid.DefaultGridBehavior());
-            this.setBehavior(new root.fin.wc.hypergrid.InMemoryGridBehavior());
-            //this.setBehavior(new root.fin.wc.hypergrid.QGridBehavior('ws://' + location.hostname + ':5000/'));
         }
     });
 }).call(this);
