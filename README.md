cdf3881d11cf3a1dabb2f601042c329949222bec
steve wirts <stevewirts@gmail.com>
2014-12-03 09:59:44 -0500

    first stab at polymer core-component-page

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 fin-hypergrid
 ================
 
-See the [component page](http://stevewirts.github.io/stevewirts/fin-hypergrid) for more information.
+See the [component page](http://openfin.github.io/fin-hypergrid) for more information.
 
 ## Getting Started
 
-We've put together a [guide for fin-hypergrid](http://www.polymer-project.org/docs/start/reusableelements.html) to help get you rolling.
+Here is a [guide for polymer](https://www.polymer-project.org/) to help get you rolling.
---
+++ gp.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -e
+#
+# @license
+# Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+# This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+# The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+# The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+# Code distributed by Google as part of the polymer project is also
+# subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+#
+
+# This script pushes a demo-friendly version of your element and its
+# dependencies to gh-pages.
+
+# usage gp Polymer core-item [branch]
+# Run in a clean directory passing in a GitHub org and repo name
+org=$1
+repo=$2
+branch=${3:-"master"} # default to master when branch isn't specified
+
+# make folder (same as input, no checking!)
+mkdir $repo
+#git clone git@github.com:$org/$repo.git --single-branch
+git clone http://github.com/$org/$repo.git --single-branch
+# switch to gh-pages branch
+pushd $repo >/dev/null
+git checkout --orphan gh-pages
+
+# remove all content
+git rm -rf -q .
+
+# use bower to install runtime deployment
+bower cache clean $repo # ensure we're getting the latest from the desired branch.
+echo "{
+  \"name\": \"$repo#gh-pages\",
+  \"private\": true
+}
+" > bower.json
+echo "{
+  \"directory\": \"components\"
+}
+" > .bowerrc
+bower install --save $org/$repo#$branch
+
+cp -rf ../../core-component-page ./components/core-component-page
+cp -rf ../../webcomponentsjs ./components/webcomponentsjs
+cp -rf ../../polymer ./components/polymer
+cp -rf ../../NodeBind ./components/NodeBind
+cp -rf ../../TemplateBinding ./components/TemplateBinding
+cp -rf ../../URL ./components/URL
+cp -rf ../../observe-js ./components/observe-js
+cp -rf ../../polymer-expressions ./components/polymer-expressions
+cp -rf ../../polymer-gestures ./components/polymer-gestures
+cp -rf ../../fin-rectangle ./components/fin-rectangle
+cp -rf ../../fin-canvas ./components/fin-canvas
+
+# redirect by default to the component folder
+echo "<META http-equiv="refresh" content=\"0;URL=components/$repo/\">" >index.html
+
+# send it all to github
+git add -A .
+git commit -am 'seed gh-pages'
+git push -u origin gh-pages --force
+
+popd >/dev/null
