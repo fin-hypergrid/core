2df6fea46ce0c3afd40d953448e63f47eaf3579d
steve wirts <stevewirts@gmail.com>
2015-04-27 10:14:45 -0400

    fixed qtree row event

--- polymer/js/features/fin-hypergrid-feature-cell-click.js
+++ polymer/js/features/fin-hypergrid-feature-cell-click.js
@@ -5,7 +5,9 @@
     Polymer('fin-hypergrid-feature-cell-click', { /* jshint ignore:line  */
         handleTap: function(grid, event) {
             var fixedRowsHeight = grid.getFixedRowsHeight();
-            if (event.primitiveEvent.detail.mouse.y > fixedRowsHeight) {
+            var fixedColsWidth = grid.getFixedColumnsWidth();
+            if ((event.primitiveEvent.detail.mouse.y > fixedRowsHeight) &&
+                (event.primitiveEvent.detail.mouse.x > fixedColsWidth)) {
                 grid.cellClicked(event);
             } else if (this.next) {
                 this.next.handleTap(grid, event);
