6ef93184c0751747e1d8abacf423709fea0d1d00
steve wirts <stevewirts@gmail.com>
2015-08-10 14:37:11 -0400

    filter field added

---
+++ test/cell-editors/fin-hypergrid-cell-editor-combo.js
@@ -0,0 +1,16 @@
+/* globals describe, it, assert */
+
+'use strict';
+
+var customElement = document.querySelector('fin-hypergrid-cell-editor-combo');
+
+describe('<fin-hypergrid-cell-editor-combo>', function() {
+
+    describe('cell-editors/fin-hypergrid-cell-editor-combo.js', function() {
+        it('should have real tests filled out', function() {
+            assert.equal(customElement, customElement);
+        });
+
+    });
+
+});
