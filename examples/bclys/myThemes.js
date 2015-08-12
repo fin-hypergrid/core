287f789593d327f4fd5b6ec35189d2936751d391
steve wirts <stevewirts@gmail.com>
2015-08-12 10:31:23 -0400

    examples pushed out

---
+++ examples/bclys/myThemes.js
@@ -0,0 +1,131 @@
+var myThemes = {
+    one: {
+        font: '8px Tahoma, Geneva, sans-serif',
+        color: 'rgb(25, 25, 25)',
+        backgroundColor: 'rgb(241, 241, 241)',
+        foregroundSelectionColor: 'rgb(255, 0, 0)',
+        backgroundSelectionColor: 'rgb(0, 0, 255)',
+
+        columnHeaderFont: '12px Tahoma, Geneva, sans-serif',
+        columnHeaderColor: 'darkblue',
+        columnHeaderBackgroundColor: 'pink',
+        columnHeaderForegroundSelectionColor: 'rgb(0, 0, 255)',
+        columnHeaderBackgroundSelectionColor: 'rgb(255, 0, 0)',
+
+        rowHeaderFont: '8px Tahoma, Geneva, sans-serif',
+        rowHeaderColor: 'rgb(25, 25, 25)',
+        rowHeaderBackgroundColor: 'rgb(223, 227, 232)',
+        rowHeaderForegroundSelectionColor: 'rgb(25, 25, 25)',
+        rowHeaderBackgroundSelectionColor: 'rgb(255, 220, 97)',
+
+        backgroundColor2: 'rgb(201, 201, 201)',
+        lineColor: 'rgb(199, 199, 199)',
+        voffset: 0,
+        scrollbarHoverOver: 'visible',
+        scrollbarHoverOff: 'hidden',
+        scrollingEnabled: true,
+
+        fixedRowAlign: 'center',
+        fixedColAlign: 'center',
+        cellPadding: 5,
+        gridLinesH: true,
+        gridLinesV: true,
+
+        defaultRowHeight: 20,
+        defaultFixedRowHeight: 15,
+        defaultColumnWidth: 100,
+        defaultFixedColumnWidth: 100,
+
+        //for immediate painting, set these values to 0, true respectively
+
+        editorActivationKeys: ['alt', 'esc'],
+        columnAutosizing: false,
+        readOnly: false
+    },
+    two: {
+        font: '10px Tahoma, Geneva, sans-serif',
+        color: 'rgb(25, 25, 25)',
+        backgroundColor: 'rgb(241, 241, 241)',
+        foregroundSelectionColor: 'rgb(25, 25, 25)',
+        backgroundSelectionColor: 'rgb(183, 219, 255)',
+
+        columnHeaderFont: '10px Tahoma, Geneva, sans-serif',
+        columnHeaderColor: 'rgb(25, 25, 25)',
+        columnHeaderBackgroundColor: 'rgb(223, 227, 232)',
+        columnHeaderForegroundSelectionColor: 'rgb(25, 25, 25)',
+        columnHeaderBackgroundSelectionColor: 'rgb(255, 220, 97)',
+
+        rowHeaderFont: '10px Tahoma, Geneva, sans-serif',
+        rowHeaderColor: 'rgb(25, 25, 25)',
+        rowHeaderBackgroundColor: 'rgb(223, 227, 232)',
+        rowHeaderForegroundSelectionColor: 'rgb(25, 25, 25)',
+        rowHeaderBackgroundSelectionColor: 'rgb(255, 220, 97)',
+
+        backgroundColor2: 'rgb(201, 201, 201)',
+        lineColor: 'rgb(199, 199, 199)',
+        voffset: 0,
+        scrollbarHoverOver: 'visible',
+        scrollbarHoverOff: 'hidden',
+        scrollingEnabled: true,
+
+        fixedRowAlign: 'center',
+        fixedColAlign: 'center',
+        cellPadding: 5,
+        gridLinesH: true,
+        gridLinesV: true,
+
+        defaultRowHeight: 15,
+        defaultFixedRowHeight: 15,
+        defaultColumnWidth: 100,
+        defaultFixedColumnWidth: 100,
+
+        //for immediate painting, set these values to 0, true respectively
+
+        editorActivationKeys: ['alt', 'esc'],
+        columnAutosizing: false,
+        readOnly: false
+    },
+    three: {
+        font: '15px Tahoma, Geneva, sans-serif',
+        color: 'rgb(25, 25, 25)',
+        backgroundColor: 'rgb(241, 241, 241)',
+        foregroundSelectionColor: 'rgb(25, 25, 25)',
+        backgroundSelectionColor: 'rgb(183, 219, 255)',
+
+        columnHeaderFont: '15px Tahoma, Geneva, sans-serif',
+        columnHeaderColor: 'rgb(25, 25, 25)',
+        columnHeaderBackgroundColor: 'rgb(223, 227, 232)',
+        columnHeaderForegroundSelectionColor: 'rgb(25, 25, 25)',
+        columnHeaderBackgroundSelectionColor: 'rgb(255, 220, 97)',
+
+        rowHeaderFont: '15px Tahoma, Geneva, sans-serif',
+        rowHeaderColor: 'rgb(25, 25, 25)',
+        rowHeaderBackgroundColor: 'rgb(223, 227, 232)',
+        rowHeaderForegroundSelectionColor: 'rgb(25, 25, 25)',
+        rowHeaderBackgroundSelectionColor: 'rgb(255, 220, 97)',
+
+        backgroundColor2: 'rgb(201, 201, 201)',
+        lineColor: 'rgb(199, 199, 199)',
+        voffset: 0,
+        scrollbarHoverOver: 'visible',
+        scrollbarHoverOff: 'hidden',
+        scrollingEnabled: true,
+
+        fixedRowAlign: 'center',
+        fixedColAlign: 'center',
+        cellPadding: 5,
+        gridLinesH: true,
+        gridLinesV: true,
+
+        defaultRowHeight: 15,
+        defaultFixedRowHeight: 15,
+        defaultColumnWidth: 100,
+        defaultFixedColumnWidth: 100,
+
+        //for immediate painting, set these values to 0, true respectively
+
+        editorActivationKeys: ['alt', 'esc'],
+        columnAutosizing: false,
+        readOnly: false
+    }
+};
