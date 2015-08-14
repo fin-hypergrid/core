dcbe69edc0a7a09a3ba1221de2db24776cfbbf2b
steve wirts <stevewirts@gmail.com>
2015-08-14 14:06:03 -0400

    row selection working

---
+++ test/features/fin-hypergrid-feature-row-selection.js
@@ -0,0 +1,16 @@
+/* globals describe, it, assert */
+
+'use strict';
+
+var customElement = document.querySelector('fin-hypergrid-feature-row-selection');
+
+describe('<fin-hypergrid-feature-row-selection>', function() {
+
+    describe('features/fin-hypergrid-feature-row-selection.js', function() {
+        it('should have real tests filled out', function() {
+            assert.equal(customElement, customElement);
+        });
+
+    });
+
+});
