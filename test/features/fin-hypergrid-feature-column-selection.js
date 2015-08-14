8eb19c831a09e218919ed76646ad784c10874b85
steve wirts <stevewirts@gmail.com>
2015-08-14 10:08:12 -0400

    fixed events

---
+++ test/features/fin-hypergrid-feature-column-selection.js
@@ -0,0 +1,16 @@
+/* globals describe, it, assert */
+
+'use strict';
+
+var customElement = document.querySelector('fin-hypergrid-feature-column-selection');
+
+describe('<fin-hypergrid-feature-column-selection>', function() {
+
+    describe('features/fin-hypergrid-feature-column-selection.js', function() {
+        it('should have real tests filled out', function() {
+            assert.equal(customElement, customElement);
+        });
+
+    });
+
+});
