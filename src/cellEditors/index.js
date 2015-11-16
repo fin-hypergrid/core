0599e4fd8370ca77d399035c36b9b0350a41039d
Jonathan Eiten <jonathan@openfin.co>
2015-11-16 12:45:45 -0500

    index.js files

---
+++ src/cellEditors/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    CellEditor: require('./CellEditor'), // abstract base class
+    Textfield: require('./Textfield'),
+    Choice: require('./Choice'),
+    //Combo: require('./Combo'),
+    Color: require('./Color'),
+    Date: require('./Date'),
+    Slider: require('./Slider'),
+    Spinner: require('./Spinner')
+};
