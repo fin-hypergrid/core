eb2843028117a2dea966a3b2ae61985e16893aa3
Dwayne Jones <dwayne@openfin.co>
2016-05-04 19:34:11 -0400

    Code review refactoring

--- src/behaviors/JSON.js
+++ src/behaviors/JSON.js
@@ -282,26 +282,10 @@ var JSON = Local.extend('behaviors.JSON', {
     },
 
     sortChanged: function(hiddenColumns){
-        var oldSorted = this.getSortedColumnIndexes(),
-            dirty = false;
-        hiddenColumns = hiddenColumns || this.getHiddenColumns();
-
-        function removeHiddenColumns(){
-            oldSorted.forEach(function(i) {
-                var j = 0,
-                    colIndex;
-                while (j < hiddenColumns.length) {
-                    colIndex = hiddenColumns[j].index + 1; //hack to get around 0 index
-                    if (colIndex === i) {
-                        hiddenColumns[j].unSort();
-                        dirty = true;
-                        break;
-                    }
-                    j++;
-                }
-            });
-        }
-        removeHiddenColumns();
+        var dirty = removeHiddenColumns(
+            this.getSortedColumnIndexes(),
+            (hiddenColumns || this.getHiddenColumns())
+        );
         if (dirty){
             this.applyAnalytics();
             this.grid.repaint();
@@ -309,5 +293,24 @@ var JSON = Local.extend('behaviors.JSON', {
     }
 
 });
+
+//Logic to moved to adapter layer outside of Hypergrid Core
+function removeHiddenColumns(oldSorted, hiddenColumns){
+    var dirty = false;
+    oldSorted.forEach(function(i) {
+        var j = 0,
+            colIndex;
+        while (j < hiddenColumns.length) {
+            colIndex = hiddenColumns[j].index + 1; //hack to get around 0 index
+            if (colIndex === i) {
+                hiddenColumns[j].unSort();
+                dirty = true;
+                break;
+            }
+            j++;
+        }
+    });
+    return dirty;
+}
 
 module.exports = JSON;
