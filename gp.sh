6626382408f582e6a2ea78f55957e08efd7b1667
Dwayne Jones <dwayne@openfin.co>
2016-05-17 15:55:41 -0400

    Updated gp to point to the new cached polymer-prototype docs

--- gp.sh
+++ gp.sh
@@ -10,58 +10,63 @@
 #
 
 # This script pushes a demo-friendly version of your element and its
-# dependencies to gh-pages.
+# dependencies to github pages
 
 # usage gp Polymer core-item [branch]
 # Run in a clean directory passing in a GitHub org and repo name
 
 org="openfin"
 repo="fin-hypergrid"
-branch="master" # default to master when branch isn't specified
+docsRepo="fin-hypergrid-polymer-demo"
+branch="polymer-prototype" # default to master when branch isn't specified
 
 #delete existing temp and recreate it, them move there
 rm -rf ../temp
 mkdir ../temp
 cd ../temp
 
-# make folder (same as input, no checking!)
-mkdir $repo
+# Make target repo
+git clone http://github.com/$org/$docsRepo.git --single-branch
 
-#git clone git@github.com:$org/$repo.git --single-branch
-git clone http://github.com/$org/$repo.git --single-branch
-
-# switch to gh-pages branch
-pushd $repo >/dev/null
+# switch to a gh-pages branch
+pushd $docsRepo
 git checkout --orphan gh-pages
 
 # remove all content
-git rm -rf -q .
+git rm -rf .
 
-# use bower to install runtime deployment
-bower cache clean $repo # ensure we're getting the latest from the desired branch.
-echo "{
-  \"name\": \"$repo#gh-pages\",
-  \"private\": true
-}
-" > bower.json
-echo "{
-  \"directory\": \"components\"
-}
-" > .bowerrc
+git clone -b $branch http://github.com/$org/$repo.git components/$repo
 
-git clone http://github.com/$org/$repo.git components/$repo
+## Update deps
+#Build Hypergrid, #vulcanize needed globally
+pushd components/$repo/
 pwd
-rm -rf components/$repo/.git
 
-# copy all dependencies while excluding this directory...
-rsync -r --exclude=temp --exclude=$repo ../../ ./components/
+npm install
+bower cache clean
+#echo "{
+#  \"name\": \"$docsRepo#gh-pages\",
+#  \"private\": true
+#}
+#" > bower.json
+#echo "{
+#  \"directory\": \"components\"
+#}
+#" > .bowerrc
+bower install
+grunt build
 
-# redirect by default to the component folder
+#Removed all hidden files
+rm -rf .*  2> /dev/null
+
+popd >/dev/null
+
+## redirect by default to the component folder
 echo "<META http-equiv="refresh" content=\"0;URL=components/$repo/\">" >index.html
 
 # send it all to github
 git add -A .
-git commit -am 'seed gh-pages'
+git commit -am 'seed old polymer docs'
 git push -u origin gh-pages --force
 
 popd >/dev/null
