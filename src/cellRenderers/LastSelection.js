d5c886410f25883f8dc1c71686c82421a5075309
Dwayne Jones <dwayne@openfin.co>
2016-06-05 17:53:00 -0400

    Turned off Selection Marquee

--- src/cellRenderers/LastSelection.js
+++ src/cellRenderers/LastSelection.js
@@ -22,18 +22,18 @@ var LastSelection = CellRenderer.extend('LastSelection', {
             width = config.bounds.width,
             height = config.bounds.height;
 
-        var focusLineStep =  [
-            [5, 5],
-            [0, 1, 5, 4],
-            [0, 2, 5, 3],
-            [0, 3, 5, 2],
-            [0, 4, 5, 1],
-            [0, 5, 5, 0],
-            [1, 5, 4, 0],
-            [2, 5, 3, 0],
-            [3, 5, 2, 0],
-            [4, 5, 1, 0]
-        ];
+        // var focusLineStep =  [
+        //     [5, 5],
+        //     [0, 1, 5, 4],
+        //     [0, 2, 5, 3],
+        //     [0, 3, 5, 2],
+        //     [0, 4, 5, 1],
+        //     [0, 5, 5, 0],
+        //     [1, 5, 4, 0],
+        //     [2, 5, 3, 0],
+        //     [3, 5, 2, 0],
+        //     [4, 5, 1, 0]
+        // ];
         gc.rect(x, y, width, height);
         gc.fillStyle = config.selectionRegionOverlayColor;
         gc.fill();
@@ -44,14 +44,14 @@ var LastSelection = CellRenderer.extend('LastSelection', {
 
         gc.stroke();
 
-        gc.rect(x, y, width, height);
-
-        gc.strokeStyle = 'white';
-
-        //animate the dashed line a bit here for fun
-        gc.setLineDash(focusLineStep[Math.floor(10 * (Date.now() / 300 % 1)) % focusLineStep.length]);
-
-        gc.stroke();
+        // gc.rect(x, y, width, height);
+        //
+        // gc.strokeStyle = 'white';
+        //
+        // //animate the dashed line a bit here for fun
+        // gc.setLineDash(focusLineStep[Math.floor(10 * (Date.now() / 300 % 1)) % focusLineStep.length]);
+        //
+        // gc.stroke();
     }
 });
 
