b58163958b05d5ee016f04c58fa64182db1b0f66
Jonathan Eiten <jonathan@openfin.co>
2016-06-17 14:01:36 -0400

    demonstrate calling setRelation with options object

--- demo/js/treedata.js
+++ demo/js/treedata.js
@@ -2,18 +2,18 @@
 
 (function() {
     var data = [
-        { ID: 0, parentID: null, name: 'USA', latitude: 36.2161472, longitude: -113.6866279 },
-        { ID: 1, parentID: 0, name: 'New York', latitude: 40.7055651, longitude: -74.118086 },
-        { ID: 2, parentID: 1, name: 'Albany', latitude: 42.6681345, longitude: -73.846419 },
-        { ID: 3, parentID: 1, name: 'Syracuse', latitude: 43.0352286, longitude: -76.1742994 },
-        { ID: 4, parentID: 0, name: 'California',  latitude: 37.1870791, longitude: -123.762638 },
-        { ID: 5, parentID: 4, name: 'Monterey', latitude: 36.5943628, longitude: -121.9025183 },
-        { ID: 6, parentID: 4, name: 'Berkeley', latitude: 37.8759458, longitude: -122.2981316 },
-        { ID: 7, parentID: 4, name: 'Laguna', latitude: 33.5482634, longitude: -117.8447927 },
-        { ID: 8, parentID: 0, name: 'Massachusetts', latitude: 42.6369691, longitude: -71.3618803 },
-        { ID: 9, parentID: 8, name: 'Lowell', latitude: 42.6369691, longitude: -71.3618803 },
-        { ID: 10, parentID: null, name: 'France', latitude: 46.1274793, longitude: -2.288454 },
-        { ID: 11, parentID: 10, name: 'Paris', latitude: 48.8588376, longitude: 2.2773459 },
+        { ID: 0, parentID: null, State: 'USA', Latitude: 36.2161472, Longitude: -113.6866279 },
+        { ID: 1, parentID: 0, State: 'New York', Latitude: 40.7055651, Longitude: -74.118086 },
+        { ID: 2, parentID: 1, State: 'Albany', Latitude: 42.6681345, Longitude: -73.846419 },
+        { ID: 3, parentID: 1, State: 'Syracuse', Latitude: 43.0352286, Longitude: -76.1742994 },
+        { ID: 4, parentID: 0, State: 'California',  Latitude: 37.1870791, Longitude: -123.762638 },
+        { ID: 5, parentID: 4, State: 'Monterey', Latitude: 36.5943628, Longitude: -121.9025183 },
+        { ID: 6, parentID: 4, State: 'Berkeley', Latitude: 37.8759458, Longitude: -122.2981316 },
+        { ID: 7, parentID: 4, State: 'Laguna', Latitude: 33.5482634, Longitude: -117.8447927 },
+        { ID: 8, parentID: 0, State: 'Massachusetts', Latitude: 42.6369691, Longitude: -71.3618803 },
+        { ID: 9, parentID: 8, State: 'Lowell', Latitude: 42.6369691, Longitude: -71.3618803 },
+        { ID: 10, parentID: null, State: 'France', Latitude: 46.1274793, Longitude: -2.288454 },
+        { ID: 11, parentID: 10, State: 'Paris', Latitude: 48.8588376, Longitude: 2.2773459 },
     ];
     window.treedata = data;
 })();
--- demo/js/treeview.js
+++ demo/js/treeview.js
@@ -8,14 +8,11 @@ var grid, behavior, dataModel;
 
 window.onload = function() {
 
-    fin.Hypergrid.analytics.util.headerify.transform = fin.Hypergrid.analytics.util.headerify.capitalize;
-
     grid = new fin.Hypergrid('div#tree-example', { data: treedata });
     behavior = grid.behavior;
     dataModel = behavior.dataModel;
 
     var idx = behavior.columnEnum;
-    var treeview;
 
     // Optional: Clone the default pipeline. If you don't do this, the mutated pipeline will be shared among all grid instances
     dataModel.pipeline = Object.getPrototypeOf(dataModel).pipeline.slice();
@@ -29,12 +26,13 @@ window.onload = function() {
 
     // Only show the data columns; don't show the ID and parentID columns
     grid.setState({
-        columnIndexes: [ idx.NAME, idx.LATITUDE, idx.LONGITUDE ],
+        columnIndexes: [ idx.STATE, idx.LATITUDE, idx.LONGITUDE ],
         checkboxOnlyRowSelections: true
     });
 
     document.querySelector('input[type=checkbox]').onclick = function() {
-        behavior.setRelation(treeview = this.checked);
+        var treeview = this.checked && { treeColumnName: 'State'};
+        behavior.setRelation(treeview);
         if (treeview) {
             dataModel.getCell = getCell;
         } else {
@@ -44,7 +42,7 @@ window.onload = function() {
 
     function getCell(config, rendererName) {
         if (config.isUserDataArea) {
-            if (config.x === idx.NAME) {
+            if (config.x === idx.STATE) {
                 config.halign = 'left';
             }
         }
