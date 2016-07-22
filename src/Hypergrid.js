65e0448cf54c7123cf319a56a174ff6abc923f1a
Colin Eberhardt <colin.eberhardt@gmail.com>
2016-07-22 16:20:24 +0100

    fix: fixes TypeError due to accessing out-of-bounds columns

--- src/Hypergrid.js
+++ src/Hypergrid.js
@@ -1257,7 +1257,7 @@ Hypergrid.prototype = {
 
         this.stopEditing(); //other editor is open, close it first
 
-        if (editPoint.x >= 0 && editPoint.y >= 0) {
+        if (editPoint.x >= 0 && editPoint.y >= 0 && editPoint.x < this.getColumnCount()) {
             var editable = this.behavior.getActiveColumn(editPoint.x).getProperties().editable;
             if (editable || this.isFilterRow(editPoint.y)) {
                 this.setMouseDown(editPoint);
