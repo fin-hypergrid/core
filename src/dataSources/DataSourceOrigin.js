bb9d858b244c590ff28c640d2bb1b25227cb654b
Dwayne Jones <dwayne@openfin.co>
2016-08-29 12:09:44 -0400

    JE PR Review #5

--- src/dataSources/DataSourceOrigin.js
+++ src/dataSources/DataSourceOrigin.js
@@ -26,13 +26,105 @@ var JSON = Null.extend('JSON',  {
         return this.data[y];
     },
 
-    findRow: function(columnName, value) {
-        var result;
-        if (value != null) {
-            result = this.data.find(function(row) { return row[columnName] === value; });
+    /**
+     * @summary Find, replace, or update a row by it's primary key column.
+     * @param {string|object} columnName - One of:
+     * * _string_ - Column name. See `value`.
+     * * _object_ - Hash of 0 or more key-value pairs to search for.
+     * @param {string[]|*} [value] - One of:
+     * _omitted_ - When `columnName` is a hash and you want to search all its keys.
+     * _string[]_ - When `columnName` is a hash but you only want to search certain keys.
+     * _otherwise_ - When `columnName` is a string. Value to search for.
+     * Note that `null` is a valid search value.
+     * @param {object|null|undefined} [replacement] - One of:
+     * * _omitted_ - Ignored.
+     * * _object_ - Replacement for the data row if found.
+     * * `null` - Flag to delete the data row if found. The found data row is nonetheless returned.
+     * * `undefined` - Flag to return index of found row instead of row object itself.
+     * @returns {object|number|undefined} One of:
+     * * `undefined` - data row not found
+     * * _object_ - found data row object (will have been deleted if `replacement` was `null`)
+     * * _number_ - index of found data row object in `this.data` (if `replacement` was `undefined`)
+     * @todo Use a binary search (rather than `Array..find`) when column is known to be indexed (sorted).
+     * @memberOf DataSource#
+     */
+    findRow: function findRow(columnName, value, replacement) {
+        var result, index, keys, hash, args;
+
+        if (typeof columnName === 'object') {
+            hash = columnName;
+
+            if (value instanceof Array) {
+                args = 2;
+                keys = value;
+                if (keys.reduce(function(sum, key) {
+                        if (key in hash) {
+                            sum++;
+                        }
+                        return sum;
+                    }, 0) !== keys.length) {
+                    throw 'Expected all keys given in 2nd arg to be found in hash given in 1st arg.';
+                }
+            } else {
+                args = 1;
+                keys = Object.keys(hash);
+                replacement = value; // promote
+            }
+
+            if (keys.length === 1) {
+                columnName = keys[0];
+                value = hash[columnName];
+                hash = undefined;
+            } else if (keys.length) {
+                result = this.data.find(function(row, idx) {
+                    if (!row) {
+                        return;
+                    }
+                    index = idx;
+                    for (var key in keys) {
+                        columnName = keys[key];
+                        if (row[columnName] !== hash[columnName]) {
+                            return; // bail
+                        }
+                    }
+                    return true; // found!
+                });
+            }
+        } else {
+            if (arguments.length < 2) {
+                throw 'Expected at least 2 arguments when first argument not object but found ' + arguments.length + '.';
+            }
+            args = 2;
         }
+
+        if (!hash) {
+            result = this.data.find(function(row, idx) {
+                if (!row) { return; }
+                index = idx;
+                return row[columnName] === value;
+            });
+        }
+
+        if (result) {
+            this.foundRowIndex = index;
+            if (replacement === null) {
+                this.data.splice(index, 1);
+            } else if (typeof replacement === 'object') {
+                this.data[index] = replacement;
+            } else if (replacement === undefined) {
+                if (arguments.length > args) {
+                    delete this.data[index];
+                }
+            } else {
+                throw 'Expected null, undefined, or object but found ' + typeof replacement + '.';
+            }
+        } else {
+            this.foundRowIndex = undefined;
+        }
+
         return result;
     },
+
 
     /**
      * @memberOf DataSource.prototype
