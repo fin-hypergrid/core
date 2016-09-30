cf1c4c4466a0def18e74cc98e0657bb8f81dbeb8
Jonathan Eiten <jonathan@openfin.co>
2016-09-30 18:59:30 -0400

    textfield cell editor respects cell halign

--- src/cellEditors/Textfield.js
+++ src/cellEditors/Textfield.js
@@ -14,6 +14,10 @@ var Textfield = CellEditor.extend('Textfield', {
 
     template: '<input type="text" lang="{{locale}}" class="hypergrid-textfield" style="{{style}}">',
 
+    initialize: function() {
+        this.input.style.textAlign = this.grid.behavior.getActiveColumn(this.editPoint.x).getCellProperty(this.editPoint.y, 'halign');
+    },
+
     localizer: Localization.prototype.string,
 
     selectAll: function() {
