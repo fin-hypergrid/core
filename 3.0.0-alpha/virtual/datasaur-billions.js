2af1f685ecfdbb19dca6ec9e4c1c7c68449b41b5
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2018-05-16 21:55:06 -0400

    alpha-3.7.5

--- 3.0.0-alpha/virtual/datasaur-billions.js
+++ 3.0.0-alpha/virtual/datasaur-billions.js
@@ -2,7 +2,7 @@
     var COLUMNS = 1000, // Math.pow(26, 1) + Math.pow(26, 2), // A..ZZ
         ROWS = 5000;
 
-    Hypergrid.modules.DatasaurMillions = Hypergrid.require('datasaur-local').extend('DatasaurMillions',  {
+    Hypergrid.modules.DatasaurBillions = Hypergrid.require('datasaur-local').extend('DatasaurBillions',  {
 
         initialize: function(datasaur, options) {
             this.cachedRowCount = 0;
@@ -10,35 +10,58 @@
         },
 
         fetchData: function(rectangles, callback) {
-            var data = this.data, schema = this.schema;
             if (this.clearCache) {
-                data = this.data = [];
+                this.data = [];
                 this.cachedRowCount = 0;
             }
-            var acc = 0;
-            rectangles.forEach(function(rectangle) {
-                for (var y = rectangle.origin.y, Y = rectangle.corner.y; y < Y; ++y) {
-                    var dataRow = data[y];
-                    if (!dataRow) {
-                        dataRow = data[y] = {};
-                        acc += 1;
-                    }
-                    for (var x = rectangle.origin.x, X = rectangle.corner.x; x < X; ++x) {
-                        var name = schema[x].name;
-                        dataRow[name] = name + ':' + y;
-                    }
-                }
-            });
-            document.getElementById('cached-rows-count').innerHTML = this.cachedRowCount += acc;
-            if (callback && Math.random() > parseInt('0' + document.getElementById('failure-rate').value) / 100) {
-                var latency = parseInt('0' + document.getElementById('latency').value);
+            if (!callback) {
+                fillRects.call(this, rectangles);
+            } else if (Math.random() > parseSetting('failure-rate') / 100) {
+                var latency = parseSetting('latency'),
+                    dataModel = this,
+                    fillAndCall = function fillRectsAndCallBack() {
+                        fillRects.call(dataModel, rectangles);
+                        callback.call(dataModel);
+                    };
                 if (latency) {
-                    setTimeout(callback.bind(this), latency);
+                    setTimeout(fillAndCall, latency);
                 } else {
-                    callback.call(this);
+                    fillAndCall();
                 }
             }
         },
+
+        // return true for all data fetched, false if any data missing
+        gotData: function(rects) {
+            var data = this.data,
+                schema = this.schema;
+
+            // for better performance, we first
+            // (1) check all rects for any missing rows before
+            // (2) checking rows for any missing cells
+            return !(
+                rects.find(function(rect) { // (1)
+                    for (var y = rect.origin.y, Y = rect.corner.y; y < Y; ++y) {
+                        var dataRow = data[y];
+                        if (!dataRow) {
+                            return true;
+                        }
+                    }
+                })
+                ||
+                rects.find(function(rect) { // (2)
+                    for (var y = rect.origin.y, Y = rect.corner.y; y < Y; ++y) {
+                        var dataRow = data[y];
+                        for (var x = rect.origin.x, X = rect.corner.x; x < X; ++x) {
+                            if (!(schema[x].name in dataRow)) {
+                                return true;
+                            }
+                        }
+                    }
+                })
+            );
+        },
+
 
         setSchema: function(newSchema){
             if (!newSchema.length) {
@@ -69,6 +92,32 @@
             return COLUMNS;
         }
     });
+
+    function fillRects(rects) {
+        var data = this.data,
+            schema = this.schema,
+            rows = 0;
+
+        rects.forEach(function(rect) {
+            for (var y = rect.origin.y, Y = rect.corner.y; y < Y; ++y) {
+                var dataRow = data[y];
+                if (!dataRow) {
+                    dataRow = data[y] = {};
+                    rows += 1;
+                }
+                for (var x = rect.origin.x, X = rect.corner.x; x < X; ++x) {
+                    var name = schema[x].name;
+                    dataRow[name] = name + ':' + y;
+                }
+            }
+        });
+
+        document.getElementById('cached-rows-count').innerHTML = this.cachedRowCount += rows;
+    }
+
+    function parseSetting(id) {
+        return parseInt('0' + document.getElementById(id).value);
+    }
 
     var BASE = 26, A = 'A'.charCodeAt();
 
