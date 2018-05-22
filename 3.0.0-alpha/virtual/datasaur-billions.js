a0bd85fb4a1c283edb59942e58cb87cf36c876ea
Jonathan Eiten <jonathan.eiten@mojotouch.com>
2018-05-22 14:08:33 -0400

    v3.0.0-alpha-3.7.8

--- 3.0.0-alpha/virtual/datasaur-billions.js
+++ 3.0.0-alpha/virtual/datasaur-billions.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var callbackOrdinal = 0;
+
 (function(Hypergrid) {
     var COLUMNS = 1000, // Math.pow(26, 1) + Math.pow(26, 2), // A..ZZ
         ROWS = 5000;
@@ -13,26 +17,27 @@
             if (this.clearCache) {
                 this.data = [];
                 this.cachedRowCount = 0;
+                this.lastSuccessfullyFetchedRects = false;
             }
-            if (!callback) {
-                fillRects.call(this, rectangles);
-            } else if (Math.random() > parseSetting('failure-rate') / 100) {
-                var latency = parseSetting('latency'),
-                    dataModel = this,
-                    fillAndCall = function fillRectsAndCallBack() {
-                        fillRects.call(dataModel, rectangles);
-                        callback.call(dataModel);
-                    };
-                if (latency) {
-                    setTimeout(fillAndCall, latency);
-                } else {
-                    fillAndCall();
-                }
-            }
+
+            fetchData.call(this, rectangles, callback);
         },
 
         // return true for all data fetched, false if any data missing
         gotData: function(rects) {
+            if (
+                this.lastSuccessfullyFetchedRects &&
+                this.lastSuccessfullyFetchedRects.length === rects.length &&
+                this.lastSuccessfullyFetchedRects.every(function(oldRect, i) {
+                    return (
+                        oldRect.origin.equals(rects[i].origin) &&
+                        oldRect.corner.equals(rects[i].corner)
+                    );
+                })
+            ) {
+                return true; // shortcut when requested rects same as last successfully fetched rects
+            }
+
             var data = this.data,
                 schema = this.schema;
 
@@ -93,13 +98,50 @@
         }
     });
 
+    function fetchData(rectangles, callback) {
+        var latency = this.latency,
+            fillAndCall = function(ordinal) {
+                if (!latency || Math.random() > this.failureRate) {
+                    // case 1: no latency (always succeeds)
+                    // case 2.1: lazy success
+                    fillRects.call(this, rectangles);
+                    this.lastSuccessfullyFetchedRects = rectangles;
+                    if (this.trace) { console.log(ordinal + ' callback'); } // observe non-deterministic order of callbacks
+                    if (callback) { callback(false); } // falsy means success (Hypergrid currently not using this value)
+                } else if (this.autoRetry) {
+                    // case 2.2: lazy retry
+                    if (this.trace) { console.log(ordinal + ' retry'); }
+                    setTimeout(fetchData.bind(this, rectangles, callback),
+                        Math.random() < .2 ? 5 * latency : latency); // simulate a data server timeout (5 x latency) as cause of failure 20% of the time;
+                } else {
+                    // case 2.3: lazy failure
+                    if (this.trace) { console.log(ordinal + ' failure'); }
+                    if (callback) { callback(true); } // truthy means error (Hypergrid currently not using this value)
+                }
+            };
+
+        if (latency) {
+            // apply latency fudge factor
+            var randomFactor = Math.random(),
+                direction = Math.random() > .5 ? 1 : -1;
+
+            latency += direction * randomFactor * latency * this.latencyDeviation;
+
+            // case 2: lazy with latency ± a randomly factored latency deviation
+            setTimeout(fillAndCall.bind(this, ++callbackOrdinal), latency);
+        } else {
+            // case 1: no latency
+            fillAndCall.call(this, ++callbackOrdinal);
+        }
+    }
+
     function fillRects(rects) {
         var data = this.data,
             schema = this.schema,
             rows = 0;
 
         rects.forEach(function(rect) {
-            for (var y = rect.origin.y, Y = rect.corner.y; y < Y; ++y) {
+            for (var y = rect.origin.y, Y = Math.min(rect.corner.y, ROWS); y < Y; ++y) {
                 var dataRow = data[y];
                 if (!dataRow) {
                     dataRow = data[y] = {};
@@ -107,22 +149,31 @@
                 }
                 for (var x = rect.origin.x, X = rect.corner.x; x < X; ++x) {
                     var name = schema[x].name;
-                    dataRow[name] = name + ':' + y;
+                    dataRow[name] = name + ':' + (y + 1);
                 }
             }
         });
 
-        document.getElementById('cached-rows-count').innerHTML = this.cachedRowCount += rows;
+        document.getElementById('cached-row-count').innerHTML = this.cachedRowCount += rows;
     }
 
-    function parseSetting(id) {
-        return parseInt('0' + document.getElementById(id).value);
+    function parseTextInput(id) {
+        return parseInt('0' + document.getElementById(id).value, 10);
+    }
+
+    // https://www.johndcook.com/blog/2010/04/29/simple-approximation-to-normal-distribution/
+    // Input: -3 <= stdDev <= +3
+    // Output: 0.0 <= value <= 1.0
+    // This rough approximation of a bell curve is NOT asymptotic.
+    // It is 2π wide so the x input conveniently gives rough standard deviations so I've called it stdDev rather than x.
+    function approxBellCurve(stdDev) {
+        return (1 + Math.cos(stdDev)) / (2 * Math.PI) * Math.PI;
     }
 
     var BASE = 26, A = 'A'.charCodeAt();
 
     function excelColumnName(x) {
-        var result = '';
+        var result = '', digits;
         for (var n = 1, range = 0, base = 0; true; n++, base = range) {
             range += Math.pow(BASE, n);
             if (x < range) {
