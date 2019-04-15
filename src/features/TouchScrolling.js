5e3f48d0f438a3227d4ce73b927cc4182b29e72a
AmirF27 <amir.f.aldeen@gmail.com>
2019-04-15 12:18:15 +0300

    Add basic horizontal touch scrolling deceleration

--- src/features/TouchScrolling.js
+++ src/features/TouchScrolling.js
@@ -27,6 +27,40 @@ var TouchScrolling = Feature.extend('TouchScrolling', {
         this.touches.push(currentTouch);
     },
 
+    handleTouchEnd: function(grid, event) {
+        var currentTouch = this.getTouchedCell(grid, event);
+        var currentTime = Date.now();
+        var timeOffset;
+        var i = this.touches.length;
+
+        do {
+            timeOffset = (currentTime - this.touches[--i].timestamp);
+        } while (timeOffset <= 100 && i > 0);
+
+        var yOffset = (currentTouch.y - this.touches[i].y);
+        var yVelocity = (Math.abs(yOffset) / timeOffset) * 100;
+        var dir = -Math.sign(yOffset);
+
+        var interval = yVelocity >= 50 ? 5 : 30;
+
+        var decelerate = function() {
+            var delta = yVelocity >= 180 ? 3 : yVelocity >= 75 ? 2 : 1;
+            grid.sbVScroller.index += dir * delta;
+
+            if (yVelocity) {
+                yVelocity = Math.max(0, yVelocity - 5);
+
+                setTimeout(decelerate, interval);
+
+                if (interval < 200) {
+                    interval += 5;
+                }
+            }
+        };
+
+        decelerate();
+    },
+
     getTouchedCell: function(grid, event) {
         var point = event.detail.touches[0];
         var cell = grid.getGridCellFromMousePoint(point).cellEvent.bounds;
