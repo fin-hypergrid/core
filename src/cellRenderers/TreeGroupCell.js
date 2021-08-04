4bc212d7e349e1b06e601338a148b335c89cef7b
Will Liu <wliu@eclipseoptions.com>
2021-08-04 17:57:50 +0800

    VC-5026 fix build error#3

--- src/cellRenderers/TreeGroupCell.js
+++ src/cellRenderers/TreeGroupCell.js
@@ -1,6 +1,6 @@
 
 var CellRenderer = require('./CellRenderer');
-var simpleCell = require('./SimpleCell.js')
+var simpleCell = require('./SimpleCell.js');
 
 /**
  * Renders a tree group cell.
@@ -9,36 +9,36 @@ var simpleCell = require('./SimpleCell.js')
  */
 // @ts-ignore TODO use classes
 
-var drillDown = '\u25bc'
-var drillRight = '\u25b6'
-var space = '    '
-var error = 'value is not valid treeInfo'
+var drillDown = '\u25bc';
+var drillRight = '\u25b6';
+var space = '    ';
+var error = 'value is not valid treeInfo';
 
 var TreeGroupCell = CellRenderer.extend('TreeGroupCell', {
-    paint: function (gc, config) {
-        config.value = CreateDisplayValue(config.value)
-        config.halign = 'left'
+    paint: function(gc, config) {
+        config.value = createDisplayValue(config.value);
+        config.halign = 'left';
 
-        simpleCell.prototype.paint(gc, config)
+        simpleCell.prototype.paint(gc, config);
     }
 });
 
-function CreateDisplayValue(treeGroup) {
+function createDisplayValue(treeGroup) {
     if (typeof treeGroup.level !== 'number' || !treeGroup.groupName) {
-        return error
+        return error;
     }
 
     if (treeGroup.isLeaf) {
-        return `${whitespace(treeGroup.level + 1)} ${treeGroup.groupName}`
+        return whitespace(treeGroup.level + 1) + ' ' + treeGroup.groupName;
     } else if (treeGroup.isExpanded) {
-        return `${whitespace(treeGroup.level)} ${drillDown} ${treeGroup.groupName}`
+        return whitespace(treeGroup.level) + ' ' + drillDown + ' ' + treeGroup.groupName;
     } else {
-        return `${whitespace(treeGroup.level)} ${drillRight} ${treeGroup.groupName}`
+        return whitespace(treeGroup.level) + ' ' + drillRight + ' ' + treeGroup.groupName;
     }
 }
 
 function whitespace(count) {
-    return space.repeat(count - 1)
+    return space.repeat(count - 1);
 }
 
 module.exports = TreeGroupCell;
