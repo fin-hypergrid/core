c403e80777603d0f1ee1e5559f77f84e99fd8ac3
Will Liu <wliu@eclipseoptions.com>
2021-08-04 17:25:55 +0800

    VC-5026 add tree group cell renderer

---
+++ src/cellRenderers/TreeGroupCell.js
@@ -0,0 +1,39 @@
+
+var CellRenderer = require('./CellRenderer');
+var simpleCell = require('./SimpleCell.js')
+
+/**
+ * Renders a tree group cell.
+ * @constructor
+ * @extends CellRenderer
+ */
+// @ts-ignore TODO use classes
+
+const drillDown = '\u25bc'
+const drillRight = '\u25b6'
+const space = '    '
+
+const CreateDisplayValue = (treeGroup) => {
+    if (treeGroup.isLeaf) {
+        return `${whitespace(treeGroup.level + 1)} ${treeGroup.groupName}`
+    } else if (treeGroup.isExpanded) {
+        return `${whitespace(treeGroup.level)} ${drillDown} ${treeGroup.groupName}`
+    } else {
+        return `${whitespace(treeGroup.level)} ${drillRight} ${treeGroup.groupName}`
+    }
+}
+
+const whitespace = (count) => {
+    return space.repeat(count - 1)
+}
+
+var TreeGroupCell = CellRenderer.extend('TreeGroupCell', {
+    paint: function (gc, config) {
+        config.value = CreateDisplayValue(config.value)
+        config.halign = 'left'
+
+        simpleCell.prototype.paint(gc, config)
+    }
+});
+
+module.exports = TreeGroupCell;
